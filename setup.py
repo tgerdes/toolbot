5b55a2b6090536e00d3c66e592e1940d75976439
Thom Gerdes <thom.gerdes@gmail.com>
2014-09-12 06:38:01 -0700

    License stuff

--- setup.py
+++ setup.py
@@ -5,5 +5,20 @@ from setuptools import setup
 setup(
     name='toolbot',
     version='1.0',
+    description="",
+    author="Thom Gerdes",
+    author_email="tgerdes@gmail.com",
     packages=['toolbot'],
+    install_requires=["aiohttp", "flask", "irc3"],
+    license="MIT",
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Flask',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4'
+        'Topic :: Communications :: Chat',
+    ),
 )
